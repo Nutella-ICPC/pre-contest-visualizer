66ada48b7f49eaaaf12ae6d7bf9e2fdd7239aab3
EnAnsari <rahmat2022a@gmail.com>
2024-05-07 03:51:32 +0330

    update: adding one json and code

---
+++ create-json/16th-West-Asia-and-newer.py
@@ -0,0 +1,73 @@
+import requests # type: ignore
+from bs4 import BeautifulSoup
+import json
+
+
+name = 'ICPC 2018 Asia Region Tehran Site'
+url = "http://icpc.sharif.edu/2018/scoreboard/"
+# url = "http://icpc.sharif.edu/2020/scoreboard/"
+response = requests.get(url)
+
+if response.status_code == 200:
+    html_content = response.text  # Get the HTML content
+else:
+    print("Error fetching webpage:", response.status_code)
+    exit()
+
+soup = BeautifulSoup(html_content, "html.parser")
+
+rankings_table = soup.find("table", id='scoreboard')  # Assuming only one table
+if not rankings_table:
+    raise Exception("Couldn't find rankings table in the HTML")
+
+header_row = rankings_table.find('thead').find('tr')
+letters = []
+for th in header_row.find_all('th')[4: -1]:
+    letter_span = th.find("span", class_="balloon")
+    if letter_span:
+        letters.append(letter_span.text.strip())
+
+my_data_html = soup.find('script')
+row_list = eval(my_data_html.string.split('=')[1].strip()[0: -1])
+
+total_teams = []
+real_rank = 1
+for team in row_list:
+    team_list = [real_rank]
+    team_list.append(team[0])
+    team_list.append(team[1].split(":")[0].strip())
+    team_list.append(team[1].split(":")[1].strip())
+    team_list.append(team[2])
+    team_list.append(team[3])
+    team_list.append(team[4])
+    accept = [0] * len(letters)
+    trying = [0] * len(letters)
+    timing = [0] * len(letters)
+    for index, q in enumerate(team[5: -1]):
+        if q != 0:
+            if q[1] == 'yes':
+                accept[index] = 1
+            elif q[1] == 'yes first':
+                accept[index] = 2
+            tr, ti = q[0].split('/')
+            trying[index] = int(tr)
+            timing[index] = int(ti) if ti != '--' else 0
+    team_list.append(accept)
+    # team_list.append(team[5])
+    team_list.append(trying)
+    team_list.append(timing)
+    total_teams.append(team_list)
+    real_rank += 1
+    # print(team_list)
+
+main_dict = {
+    'name': name,
+    'url': url,
+    'question_names': letters,
+    'teams': total_teams,
+}
+
+with open(f"./jsons/{'-'.join(name.split())}.json", "w") as outfile:
+    # json.dump(main_dict, outfile, indent=4)
+    json.dump(main_dict, outfile)
+
